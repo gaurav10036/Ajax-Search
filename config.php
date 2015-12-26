96256ad3cd7fe3d8b9dc5e1a7f51e98eaeb3eb40
Gaurav Sirauthiya <gaurav.sirauthiya91@gmail.com>
2015-12-27 00:42:43 +0530

    add

--- config.php
+++ config.php
@@ -1,3 +1,3 @@
-<?php $con = mysql_connect('localhost','root','admin123') or die(mysql_error());
+<?php $con = mysql_connect('localhost','root','a123') or die(mysql_error());
 	  $db = mysql_select_db('sakila') or die(mysql_error());
  ?>
